dfd8ea7a4c6f104265480725014cc60a374a6556
Hend Ismail <hend.m.ismail@gmail.com>
2018-10-17 13:12:19 +0200

    first commit

---
+++ src/main/java/example/HelloWorld.java
@@ -0,0 +1,10 @@
+package example;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+
+public class HelloWorld implements RequestHandler<Object, String>{
+    public String handleRequest(Object input, Context context) {
+        return "Received Object :  " + input.toString();
+    }
+}
